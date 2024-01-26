33bd78414a60a88eeb9e348ecc76e98d50ac5beb
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-01-26 00:16:57 -0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# This file contains commands that will be executed at the end of
+# EndeavourOS install (online mode only) on the target system.
+# The commands will be executed as root.
+#
+# This allows you to customize the installed system in several ways!
+#
+# If you add commands to this file, start the install process after
+# saving this file.
+#
+# Tip: save your customized commands into a file on an internet server
+# and fetch that file with command:
+#
+#     wget -O ~/user_commands.bash "URL-referring-the-file"
+#
+# Ideas for customization:
+#   - install packages
+#   - remove packages
+#   - enable or disable system services
+#   - writing dotfiles under $HOME
+#   - etc.
+#
+# Example commands:
+#     pacman -S --noconfirm --needed gufw geany chromium
+#     pacman -Rsn --noconfirm xed
+#     systemctl enable ufw
+#
+# There are some limitations to the commands:
+#   - The 'pacman' commands mentioned above require option '--noconfirm',
+#     otherwise the install process may hang because pacman waits for a
+#     confirmation!
+#   - Installing packages with 'yay' does not work because yay may not
+#     be run as root.
+#     The 'makepkg' command suffers from the same limitation.
+#     This essentially blocks installing AUR packages here.
+#
+# Advanced tip (for ISOs since year 2022):
+#    To write files directly into $HOME, you can find the new username
+#    as the first parameter given to user_commands.bash, e.g.
+#        username="$1"
+#    Then you may write files under folder
+#        /home/$username
+#
+#----------------------------------------------------------------------------------
+#
+# Now *you* can add your commands into the two functions below.
+#
+#----------------------------------------------------------------------------------
+
+_IsoConfig() {
+    # ISO configurations here.
+    # This runs before calamares is started.
+
+    local -r install_mode="$1"          # 'online' or 'offline'
+    # ...
+    # ls /home/liveuser
+}
+
+_PostInstallCommands() {
+    # Post-install commands here.
+    # This runs near the end of calamares execution.
+
+    local -r username="$1"              # new user you created for the target
+    # ...
+     echo "# Fucking Genius!" >> /home/$username/.bashrc
+     pacman -S --noconfirm --needed geany chromium libreoffice-fresh xtra-cmake-modules boost python-jsonschema python-unidecode gawk qt5-svg qt5-webengine yaml-cpp networkmanager upower  
+     squashfs-tools rsync cryptsetup qt5-xmlpatterns doxygen dmidecode gptfdisk hwinfo polkit-qt5  
+     solid5 qt5-tools boost-libs libpwquality ckbcomp qt5-quickcontrols2
+    useradd -m -G wheel -s /bin/bash build
+}
+
+case "$1" in
+    --iso-config)        _IsoConfig "$2" ;;
+    offline | online)    _IsoConfig "$1" ;;            # after Galileo neo!
+    *)                   _PostInstallCommands "$1" ;;
+esac
